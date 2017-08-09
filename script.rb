afaed1f1adc703f8fd65504819259d66cfeb2cdc
mpd <mpd@jesters-court.net>
2017-08-08 23:19:29 -0700

    refactor to classes

--- script.rb
+++ script.rb
@@ -13,6 +13,56 @@ Material = Struct.new(:color)
 Ray = Struct.new(:origin, :direction)
 Light = Struct.new(:origin, :color)
 
+# Scene Model
+class Scene
+  extend Forwardable
+  def_delegators :@canvas, :display, :write
+
+  attr_reader :models
+  def initialize(models, width: 512, height: 512, fov: 90)
+    @models = models
+    @width = width
+    @height = height
+    @fov = fov
+    @aspect_ratio = if width > height
+                      width.to_f / height.to_f
+                    else
+                      height.to_f / width.to_f
+                    end
+    @fov_radians = (fov.to_f * Math::PI) / 180.0
+    @fov_adjustment = Math.tan(@fov_radians / 2.0)
+    @canvas = Magick::Image.new(width, height) do |img|
+      img.background_color = 'black'
+    end
+  end
+
+  def render
+    draw = Magick::Draw.new
+
+    # :orthographic vs perspective
+    @height.times do |y|
+      @width.times do |x|
+        # take center of pixel, then normalize to (-1.0..1.0), then adjust for fov
+        ray_x = (((x + 0.5) / @width) * 2.0 - 1.0) * @fov_adjustment
+        ray_y = (1.0 - ((y + 0.5) / @height) * 2.0) * @fov_adjustment
+        if @width > @height
+          ray_x *= @aspect_ratio
+        else
+          ray_y *= @aspect_ratio
+        end
+        ray = Ray.new(Point[0.0, 0.0, 0.0], Direction[ray_x, ray_y, -1.0].normalize)
+
+        if @models[0].intersects_with?(ray)
+          draw.fill(@models[0].material)
+          draw.point(x, y)
+        end
+      end
+    end
+
+    draw.draw(@canvas)
+  end
+end
+
 # Sphere model
 class Sphere
   attr_reader :origin, :radius, :material
@@ -21,14 +71,20 @@ class Sphere
   #
   # @param origin [Point]
   # @param radius [Float]
-  # @param material [Material]
+  # @param material [Material] - Anything that ImageMagick can handle.
+  #   See http://www.simplesystems.org/RMagick/doc/draw.html#fill
   # @return [Sphere]
-  def initialize(origin, radius, material)
+  def initialize(origin, radius, material = 'white')
     @origin = origin
     @radius = radius
     @material = material
   end
 
+  # Where do I intersect with the given ray?
+  #
+  # @param [Ray] - the ray to check against
+  # @return [Point] - nearest point of intersection
+  # @return [nil] - no intersection
   def intersects_with?(ray)
     hyp = origin - ray.origin
     adj = hyp.dot(ray.direction)
@@ -37,43 +93,9 @@ class Sphere
   end
 end
 
-width = 1024
-height = 512
-fov = 90
-aspect_ratio = if width > height
-                 width.to_f / height.to_f
-               else
-                 height.to_f / width.to_f
-               end
-fov_radians = (fov.to_f * Math::PI) / 180.0
-fov_adjustment = Math.tan(fov_radians / 2.0)
-
-sphere = Sphere.new(Point[0.0, 0.0, -2.0], 1.0, Material.new('black'))
-
-canvas = Magick::Image.new(width, height) do |img|
-  img.background_color = 'blue'
-end
-
-draw = Magick::Draw.new
-draw.fill('yellow')
-
-# :orthographic vs perspective
-height.times do |y|
-  width.times do |x|
-    # send ray through center of pixel, then normalize to (-1.0..1.0)
-    ray_x = (((x + 0.5) / width) * 2.0 - 1.0) * fov_adjustment
-    ray_y = (1.0 - ((y + 0.5) / height) * 2.0) * fov_adjustment
-    if width > height
-      ray_x *= aspect_ratio
-    else
-      ray_y *= aspect_ratio
-    end
-    ray = Ray.new(Point[0.0, 0.0, 0.0], Direction[ray_x, ray_y, -1.0].normalize)
-    draw.point(x, y) if sphere.intersects_with?(ray)
-  end
-end
-
-draw.draw(canvas)
-# binding.pry
-
-canvas.display
+models = [
+  Sphere.new(Point[0.0, 0.0, -2.0], 1.0, '#00ff0033')
+]
+scene = Scene.new(models, width: 666)
+scene.render
+scene.display
